6989d8e3358e5c9db8467807dd54853eeae518d0
filMolteni <filippo.molteni2005@gmail.com>
2026-05-12 11:23:52 +0200

    aggiunta driver mysql e sistemato errore connessione db

--- src/server/utils/DBConnectionPool.java
+++ src/server/utils/DBConnectionPool.java
@@ -10,16 +10,21 @@ public class DBConnectionPool {
 
     public static void initialize(String host, String dbName, String user, String password) {
         try {
+            // Carica il driver MySQL
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
             pool = new ArrayBlockingQueue<>(10);
 
-            String url = "jdbc:postgresql://" + host + "/" + dbName;
+            // URL per MySQL su XAMPP
+            String url = "jdbc:mysql://" + host + "/" + dbName + "?useSSL=false&serverTimezone=UTC";
 
+            // Crea 10 connessioni e mettile nel pool
             for (int i = 0; i < 10; i++) {
                 Connection conn = DriverManager.getConnection(url, user, password);
                 pool.add(conn);
             }
 
-            System.out.println("Pool di connessioni inizializzato.");
+            System.out.println("Pool di connessioni MySQL inizializzato correttamente.");
 
         } catch (Exception e) {
             throw new RuntimeException("Errore inizializzazione pool DB", e);
